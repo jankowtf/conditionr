af00945e4397b1f5b5f475b777a0af5a88b7e913
Janko Thyson <janko.thyson@gmail.com>
2014-09-02 22:40:46 +0200

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   .Rbuildignore
    	new file:   .gitignore
    	new file:   DESCRIPTION
    	new file:   NAMESPACE
    	new file:   R/.onLoad.r
    	new file:   R/rapp.core.condition.R
    	new file:   R/signalCondition.r
    	new file:   inst/examples/signalCondition.r
    	new file:   man-roxygen/author.r
    	new file:   man-roxygen/context-and-namespace.R
    	new file:   man-roxygen/general-remarks.r
    	new file:   man-roxygen/references.r
    	new file:   man-roxygen/see-generic.R
    	new file:   man-roxygen/threedot.r
    	new file:   man/rapp.core.condition-package.Rd
    	new file:   man/signalCondition-character-method.Rd
    	new file:   man/signalCondition-missing-method.Rd
    	new file:   man/signalCondition.Rd
    	new file:   rapp.core.condition.Rproj
    	new file:   tests/testthat/test-signalCondition.r

---
+++ tests/testthat/test-signalCondition.r
@@ -0,0 +1,75 @@
+context("package")
+
+test_that("signalCondition", {
+
+    expect_is(res <- signalCondition(), "RappDefaultMessage")
+    expect_warning(
+      expect_is(res <- signalCondition(type = "warning"), "RappDefaultWarning")
+    )
+    expect_error(res <- signalCondition(type = "error"))
+    
+    expect_is(res <- signalCondition(signal = FALSE), "RappDefaultMessage")
+    expect_is(res <- signalCondition(type = "warning", signal = FALSE), 
+      "RappDefaultWarning")
+    expect_is(res <- signalCondition(type = "error", signal = FALSE),
+      "RappDefaultError")
+    
+    expect_is(res <- signalCondition(
+      condition = "TestCondition"), "TestCondition")
+    expect_true(inherits(res, "message"))
+    expect_warning(
+        expect_is(res <- signalCondition(
+          condition = "TestCondition",
+          type = "warning"
+        ), "TestCondition")
+    )
+    expect_true(inherits(res, "warning"))
+    expect_error(res <- signalCondition(
+      condition = "TestCondition", 
+      type = "error")
+    )
+    
+    expect_is(res <- signalCondition(
+        condition = "TestCondition",
+        signal = FALSE
+      ), "TestCondition")
+    expect_true(inherits(res, "message"))
+    expect_is(res <- signalCondition(
+        condition = "TestCondition",
+        type = "warning",
+        signal = FALSE
+      ), "TestCondition")
+    expect_true(inherits(res, "warning"))
+    expect_is(res <- signalCondition(
+        condition = "TestCondition", 
+        type = "error",
+        signal = FALSE
+      ), "TestCondition"
+    )
+    expect_true(inherits(res, "error"))
+    
+    expect_is(res <- signalCondition(
+        condition = "TestCondition",
+        msg = c("Header", "Line 1" = "line 1", "Line 2" = "line 2"),
+        signal = FALSE
+      ), "TestCondition")
+    expect_match(paste(res, collapse="\n"), "Line 1")
+    expect_is(res <- signalCondition(
+        condition = "TestCondition",
+        msg = c("Header", "Line 1" = "line 1", "Line 2" = "line 2"),
+        type = "warning",
+        signal = FALSE
+      ), "TestCondition")
+    expect_match(paste(res, collapse="\n"), "Line 1")
+    expect_is(res <- signalCondition(
+        condition = "TestCondition", 
+        msg = c("Header", "Line 1" = "line 1", "Line 2" = "line 2"),
+        type = "error",
+        signal = FALSE
+      ), "TestCondition"
+    )
+    expect_match(paste(res, collapse="\n"), "Line 1")
+    expect_true(inherits(res, "error"))
+    
+  }
+)
